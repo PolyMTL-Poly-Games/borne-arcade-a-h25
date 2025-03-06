2476b47afa4f3f00396d7de35a4e63ef7112f237
Steven Ho <steven.ho01@outlook.com>
2025-03-06 01:20:05 -0500

    Implemented partially player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,31 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float moveSpeed = 10f;      // Horizontal movement speed
-    public float jumpForce = 20f;    // Force applied for jumping
+    public float jumpForce = 18f;    // Force applied for jumping
 
+    private Animator animator;
     private Rigidbody2D rb;
     private bool isGrounded;
     private bool canWallJump;
     private Vector2 wallNormalDirection;
+    private bool isFacingRight = true;
 
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float moveInput = Input.GetAxis("Horizontal");
+        handleMovementAnimation(moveInput);
+
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
         if (Input.GetButtonDown("Jump"))
@@ -43,10 +49,9 @@ public class PlayerController : MonoBehaviour
             canWallJump = false;
 
         }
-        else if (collision.gameObject.CompareTag("Wall"))
+        else if (collision.gameObject.CompareTag("Wall") && !isGrounded)
         {
             canWallJump = true;
-            isGrounded = false;
         }
     }
 
@@ -57,5 +62,23 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
             canWallJump = false;
         }
+    }
+
+    // Je dois résoudre des bugs
+    private void handleMovementAnimation(float moveInput)
+    {
+        if ((moveInput > 0 && !isFacingRight) || (moveInput < 0 && isFacingRight))
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+
+        animator.SetFloat("isRunning", Math.Abs(moveInput));
+        animator.SetBool("isFalling", rb.linearVelocity.y < 0);
+        animator.SetBool("isJumping", !isGrounded);
+        Debug.Log(moveInput);
+        animator.SetBool("isHoldingWall", canWallJump && Math.Abs(moveInput) > 0);
     }
 }
