ba2a6292e85487338ca9a502b522053cc34a8ca5
Johnson-Ho20 <Johnson_Ho98@hotmail.com>
2025-03-30 22:38:05 -0400

    end credit

---
+++ Assets/GameOverTrigger.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameOverTrigger : MonoBehaviour
+{
+    public GameObject gameOverUI;
+
+    private void Start()
+    {
+        gameOverUI.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Time.timeScale = 0f;
+            gameOverUI.SetActive(true);
+        }
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
